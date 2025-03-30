b7cd23a26c260651bbcbac5a36a565da7866b504
Dinesh Ketana <ketanadinesh.22.it@anits.edu.in>
2025-03-30 22:52:16 +0530

    files

--- factorial.java
+++ factorial.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
 public class factorial {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int n = sc.nextInt();
-        int fact = 1;
-        for (int i = 1; i <= n; i++) {
-            fact = fact * i;
+        if (args.length == 0) {
+            System.out.println("Usage: java Factorial <number>");
+            return;
         }
-        System.out.println("Factorial of " + n + " is " + fact);
+
+        int num = Integer.parseInt(args[0]);
+        int fact = 1;
+
+        for (int i = 2; i <= num; i++) fact *= i;
+
+        System.out.println("Factorial of " + num + " is: " + fact);
     }
 }
